6fe8b84c7cbf82e8081957a69d37c737f04cf736
CodinGitHub <gms2programador@gmail.com>
2021-07-23 04:28:41 -0500

    primera forma de crear una fecha

---
+++ main.js
@@ -0,0 +1,10 @@
+let fechaActual = new Date();
+console.log(fechaActual);
+console.log(fechaActual.getDate());
+console.log(fechaActual.getMonth());
+console.log(fechaActual.getFullYear());
+
+//TIMESTAMP
+console.log(fechaActual.getTime());
+console.log(Date.now());
+console.log(+fechaActual)
