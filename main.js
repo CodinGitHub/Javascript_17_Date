d807f373cb6a33d52f36bdcabf2486f119fe1e6c
CodinGitHub <gms2programador@gmail.com>
2021-07-24 05:23:19 -0500

    first commit

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+// Date()
 let fechaActual = new Date();
 console.log(fechaActual);
 console.log(fechaActual.getDate());
 console.log(fechaActual.getMonth());
 console.log(fechaActual.getFullYear());
+console.log(fechaActual.getDay());
 
 //TIMESTAMP
 console.log(fechaActual.getTime());
 console.log(Date.now());
 console.log(+fechaActual)
+
+let fechaConTimestamp = new Date(1627033033900);
+console.log(fechaConTimestamp);
+
+//STRING
+let fechaConString = new Date("2021 jan 23");
+console.log(fechaConString);
+
+// Date(año, mes, fecha, horas, minutos, segundos, ms)
